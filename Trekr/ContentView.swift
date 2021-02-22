b69007042fa7aeebe50dfb2c2ade14b067c70d2e
Aldinonid <aldino.efendi7@gmail.com>
2021-02-23 00:30:05 +0700

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Aldino Efendi on 23/02/3 R.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Aldino Efendi on 23/02/3 R.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
